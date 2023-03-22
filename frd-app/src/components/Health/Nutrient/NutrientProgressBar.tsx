d34c98127abaaff331bfa3c345d376c100cf3994
hinwong1021@gmail.com <hinwong1021@gmail.com>
2023-03-22 23:14:15 +0800

    add progress bar function, update Nutrient.tsx

---
+++ frd-app/src/components/Health/Nutrient/NutrientProgressBar.tsx
@@ -0,0 +1,27 @@
+import React, { useState, useEffect } from "react";
+import "./NutrientProgressBar.css"; // Import the CSS file for the progress bar
+
+type ProgressBarProps = {
+  dailyIntake: number;
+  currentIntake: number;
+};
+
+export function NutrientProgressBar({ dailyIntake, currentIntake }: ProgressBarProps) {
+  const [progress, setProgress] = useState<number>(0);
+
+  useEffect(() => {
+    // Calculate the progress towards the daily intake
+    const progress = (currentIntake / dailyIntake) * 100;
+
+    // Update the width of the progress bar
+    setProgress(progress);
+  }, [currentIntake, dailyIntake]);
+
+  return (
+    <>
+      <div className="progress-bar">
+        <div className="nutrient-fill" style={{ width: `${progress}%` }}></div>
+      </div>
+    </>
+  );
+}
