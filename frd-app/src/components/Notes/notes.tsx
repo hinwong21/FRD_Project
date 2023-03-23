c99e2a48a0703b5c6c22595b5d443a0c25bc42ab
Gay <paul6541yau@gmail.com>
2023-03-23 11:44:06 +0800

    Merge origin

--- frd-app/src/components/Notes/notes.tsx
+++ frd-app/src/components/Notes/notes.tsx
@@ -1,17 +1,33 @@
-import React from "react";
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faPlus } from '@fortawesome/free-solid-svg-icons'
-import styles from "./Notes.module.css"
-import { IonTitle } from "@ionic/react";
+import {
+    IonButtons,
+    IonContent,
+    IonHeader,
+    IonMenuButton,
+    IonPage,
+    IonTitle,
+    IonToolbar,
+  } from "@ionic/react";
+  import { useParams } from "react-router";
+  import styles from "./Calendar.module.css"
+  
+// 顯示所有notes，包括diary, memos, todolists (sql get)
 
-
-export const Notes: React.FC = () => {
-  return (
-    <> 
-      <div className={styles.addNotes}><FontAwesomeIcon icon = {faPlus} /></div>
-    </>
-   
+  export const Notes: React.FC = () => {
+    const { name } = useParams<{ name: string }>();
+    // console.log(name);
+    let titleName = "";
+    let fetchPage = <></>;
+  
+    return (
+      <IonPage>
+        <IonContent fullscreen>
+          <div className={styles.calendarWrapper}>
+          </div>
+          {/* Fetch to the page that name equal to url */}
+          {fetchPage}
+        </IonContent>
+      </IonPage>
     );
-};
-
-export default Notes
+  };
+  
+  export default Notes;
