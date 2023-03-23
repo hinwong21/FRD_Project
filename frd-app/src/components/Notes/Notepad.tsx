32909a07af33151989cf1912e20c3f4ec98a7da9
Cynthia Lo <cynthiazlo811@gmail.com>
2023-03-23 16:17:29 +0800

    updated file

--- frd-app/src/components/Notes/Notepad.tsx
+++ frd-app/src/components/Notes/Notepad.tsx
@@ -16,7 +16,7 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faPlus } from "@fortawesome/free-solid-svg-icons";
 import styles from "./Notes.module.css";
 import "./Notes.module.css";
-import {AddNotePopup} from "./AddNotePopup"
+import { AddNotePopup } from "./AddNotePopup";
 // import { useParams } from "react-router";
 
 export const Notepad: React.FC = () => {
@@ -26,12 +26,10 @@ export const Notepad: React.FC = () => {
   let fetchPage = <></>;
 
   return (
-  <>
-  < AddNotePopup />
-  </>
+    <>
+      <AddNotePopup />
+    </>
   );
 };
-
-
 
 export default Notepad;
