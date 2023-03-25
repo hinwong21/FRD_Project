3d5a66e56a4a87658edb55ed51fdea4da1edf6ad
Alva Suen <ymsym98@gmail.com>
2023-03-25 10:59:49 +0800

    Add new files

---
+++ frd-app/src/components/Notes/TodoListEditor.tsx
@@ -0,0 +1,17 @@
+import { IonPage, IonContent } from "@ionic/react";
+import React, { useEffect, useState, Component } from "react";
+import { Editor } from "react-draft-wysiwyg";
+import { convertFromRaw } from "draft-js";
+
+
+export const TodoListEditor: React.FC = () => {
+  return (
+    <IonPage>
+      <IonContent fullscreen>
+      
+      </IonContent>
+    </IonPage>
+  );
+};
+
+export default TodoListEditor;
---
+++ frd-app/src/redux/Calendar/reducer.ts
@@ -0,0 +1,3 @@
+import { AnyAction } from "redux";
+import { CalendarEventAction } from "./action";
+import { initialState, CalendarEventState } from "./state";
