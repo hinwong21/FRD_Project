d41f2b93748a637f12c1631097665f1501072b8c
Cynthia Lo <cynthiazlo811@gmail.com>
2023-04-01 13:15:35 +0800

    modified LevelBtn.tsx, PeriodDay.tsx, StatusItem.tsx and AddNotePopup.tsx. Also deleted MemoEditor.tsx

--- frd-app/src/components/Notes/AddNotePopup.tsx
+++ frd-app/src/components/Notes/AddNotePopup.tsx
@@ -1,67 +1,82 @@
-import React from "react";
-import {
-  IonButtons,
-  IonButton,
-  IonPopover,
-  IonContent,
-  IonList,
-  IonItem,
-  IonNavLink,
-  IonFab,
-  IonFabButton,
-  IonFabList
-} from "@ionic/react";
+import React, { useState } from "react";
+import { IonButton, IonFab, IonFabButton, IonFabList } from "@ionic/react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faPlus, faBook, faPen, faCheck} from "@fortawesome/free-solid-svg-icons";
-import  DiaryEditor  from "./DiaryEditor";
-import TodoListEditor from "./TodoListEditor"
-import MemoEditor from "./MemoEditor";
+import {
+  faPlus,
+  faBook,
+  faPen,
+  faCheck,
+} from "@fortawesome/free-solid-svg-icons";
+import { DiaryEditor } from "./DiaryEditor";
+import { TodoListEditor } from "./TodoListEditor";
+import MemoEditor, { TextEditor } from "./TextEditor";
 
 import styles from "./Notes.module.css";
 import "./Notes.module.css";
+import { OverlayEventDetail } from "@ionic/react/dist/types/components/react-component-lib/interfaces";
 
 export const AddNotePopup: React.FC = () => {
+  const [diaryOpen, setDiaryOpen] = useState(false);
+
   const list = {
     options: [
       {
         icon: <FontAwesomeIcon icon={faBook} />,
         text: "Diary",
-        redirect: <DiaryEditor/>
       },
       {
         icon: <FontAwesomeIcon icon={faPen} />,
         text: "Memo",
-        redirect: <MemoEditor/>
       },
       {
         icon: <FontAwesomeIcon icon={faCheck} />,
-        text: "Todo List",
-        redirect: <TodoListEditor/>
+        text: "TodoList",
       },
     ],
   };
-  
 
-    return (
-      <>
+  // function openEditor(editorType:string){
+  //   console.log(editorType)
 
+  // }
 
-        <IonFab slot="fixed" vertical="bottom" horizontal="end" className={styles.fabButton}>
-          <IonFabButton>
-            <FontAwesomeIcon icon={faPlus} />
-          </IonFabButton>
-          <IonFabList side="top">
+  // const [present, dismiss] = useIonModal(NewEventForm, {
+  //   onDismiss: (data: string, role: string) => dismiss(data, role),
+  // });
+
+  // function openModal() {
+  //   present({
+  //     onWillDismiss: (ev: CustomEvent<OverlayEventDetail>) => {
+  //       if (ev.detail.role === "confirm") {
+  //         // setMessage(`Hello, ${ev.detail.data}!`);
+  //       }
+  //     },
+  //   });
+  // }
+
+  return (
+    <>
+      <IonFab
+        slot="fixed"
+        vertical="bottom"
+        horizontal="end"
+        className={styles.fabButton}
+      >
+        <IonFabButton>
+          <FontAwesomeIcon icon={faPlus} />
+        </IonFabButton>
+        <IonFabList side="top">
           {list.options.map((item, index) => (
-                <IonFabButton key={index}>
-                  <IonNavLink routerDirection="forward" component={() => <DiaryEditor/>}>
-                  <div>{list.options[index].icon}</div>
-                  </IonNavLink>
-                </IonFabButton>
-              ))}
-          </IonFabList>
-        </IonFab>
-      </>
-    );
-  };
+            <IonFabButton key={index}>
+              <IonButton>
+                <div>{list.options[index].icon}</div>
+              </IonButton>
+            </IonFabButton>
+          ))}
+        </IonFabList>
+      </IonFab>
+    </>
+  );
+};
 
 export default AddNotePopup;
