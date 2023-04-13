61ee78930d685f7072333a0a2299eea898b49416
hinwong1021@gmail.com <hinwong1021@gmail.com>
2023-04-14 01:08:05 +0800

    IonSelect change to onIonChange

--- frd-app/src/components/Set/LoginSetup.tsx
+++ frd-app/src/components/Set/LoginSetup.tsx
@@ -1,4 +1,4 @@
-import React, { useMemo, useState } from "react";
+import React, { useState } from "react";
 import styles from "./LoginSetup.module.css";
 import { getName } from "../../service/LocalStorage/LocalStorage";
 import { Preferences } from "@capacitor/preferences";
