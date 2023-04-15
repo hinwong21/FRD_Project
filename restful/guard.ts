6ce3f704ee0a6c5e0ed83e7c1f88afd435cfd3e3
Gay <paul6541yau@gmail.com>
2023-04-15 23:17:42 +0800

    improve error message when token is not given

--- restful/guard.ts
+++ restful/guard.ts
@@ -1,27 +1,31 @@
 import express from "express";
 import { errorHandler } from "./error";
-import "./session"
+import "./session";
 import { verifyJwt } from "./jwt";
 
-
-export const isLoggedInAPI = async (req: express.Request, res: express.Response, next: express.NextFunction) => {
-    try {
-
-        if (req.headers.authorization && req.headers.authorization.split(' ')[0] === 'Bearer') {
-            let token = req.headers.authorization.split(' ')[1];
-            verifyJwt(token, req);
-            next()
-        } else {
-            throw new Error("Not exist token in header")
-        }
-
-    } catch (err) {
-        // res.json({
-        //     data:null,
-        //     isErr:true,
-        //     errMess:err.message
-        // })
-        errorHandler(err, req, res)
+export const isLoggedInAPI = async (
+  req: express.Request,
+  res: express.Response,
+  next: express.NextFunction
+) => {
+  try {
+    if (
+      req.headers.authorization &&
+      req.headers.authorization.split(" ")[0] === "Bearer"
+    ) {
+      let token = req.headers.authorization.split(" ")[1];
+      if (!token) throw new Error("Not exist token in header");
+      verifyJwt(token, req);
+      next();
+    } else {
+      throw new Error("Not exist token in header");
     }
-
+  } catch (err) {
+    // res.json({
+    //     data:null,
+    //     isErr:true,
+    //     errMess:err.message
+    // })
+    errorHandler(err, req, res);
+  }
 };
